c270b9835c09d58460537a23481e53735bf0382a
Aleksandr Moskalchuk <moskalchuk.aleksandr@gmail.com>
2022-03-21 23:10:10 +0300

    Initial commit

---
+++ src/main/java/org/config/Application.java
@@ -0,0 +1,9 @@
+package org.config;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
